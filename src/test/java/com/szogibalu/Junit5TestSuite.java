370a0c27792840d812df280bf0201cd23568ede3
szogi <szogibalu@gmail.com>
2016-10-31 16:17:07 +0100

    Init project

---
+++ src/test/java/com/szogibalu/Junit5TestSuite.java
@@ -0,0 +1,11 @@
+package com.szogibalu;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.runner.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("com.szogibalu")
+public class Junit5TestSuite {
+	
+}
