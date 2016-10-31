cb456132dbb88b076d594bb71663fd5be8700dd7
szogi <szogibalu@gmail.com>
2016-10-31 16:58:21 +0100

    Polishing

---
+++ src/test/java/com/szogibalu/JUnit4Test.java
@@ -0,0 +1,12 @@
+package com.szogibalu;
+
+import org.junit.Test;
+
+public class JUnit4Test {
+
+	@Test
+	public void test() {
+
+	}
+
+}
---
+++ src/test/java/com/szogibalu/JUnit5Test.java
@@ -0,0 +1,12 @@
+package com.szogibalu;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnit5Test {
+
+	@Test
+	void test() {
+
+	}
+
+}
